75cd24c90cd038f2bce3f112c7361ec1242c191f
Navneet <behlnavneet@gmail.com>
2020-10-19 14:25:18 +0100

    First commit for RabbitMQ

---
+++ src/main/java/com/behl/timotious/RabbitMqConsumerNewApplication.java
@@ -0,0 +1,13 @@
+package com.behl.timotious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqConsumerNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqConsumerNewApplication.class, args);
+	}
+
+}
